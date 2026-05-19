b0ff62982e9d7b846cd5f7c67124defdbdb4fa8c
TopScrech <sergei_saliukov@icloud.com>
2026-05-19 09:48:05 +0200

    swift-tools-version: 6.2.1 -> 6.3.2 & updated readme

--- Package.swift
+++ Package.swift
@@ -1,16 +1,24 @@
-// swift-tools-version:6.2.1
+// swift-tools-version:6.3.2
 
 import PackageDescription
 
 let package = Package(
     name: "Attester",
-    platforms: [.iOS(.v14), .macOS(.v10_15)],
+    platforms: [
+        .iOS(.v14),
+        .macOS(.v10_15)
+    ],
     products: [
         .library(name: "Attester", targets: ["Attester"])
     ],
     dependencies: [
+        // https://github.com/myfreeweb/SwiftCBOR
         .package(url: "https://github.com/myfreeweb/SwiftCBOR", from: "0.4.6"),
+        
+        // https://github.com/apple/swift-crypto
         .package(url: "https://github.com/apple/swift-crypto", from: "3.1.0"),
+        
+        // https://github.com/iansampson/Anchor
         .package(url: "https://github.com/iansampson/Anchor", branch: "main")
     ],
     targets: [
